6fe829d73fd667a070da3878d5d8792335ab12a6
Dave McCloskey <davemccloskey@gmail.com>
2013-09-28 16:37:12 -0700

    Loads images from a static value in controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,4 +5,12 @@
 angular.module('flickrpedia.controllers', []).
   controller('Main', ['$scope', '$routeParams', function($scope, $routeParams) {
     $scope.term = $routeParams.term;
+    $scope.results = [
+      {
+        url: 'http://farm6.staticflickr.com/5248/5229241621_75fddaa50e_b.jpg'
+      },
+      {
+        url: 'http://farm5.staticflickr.com/4005/4204497503_cacf3abe74_o.jpg'
+      }
+    ];
   }]);
