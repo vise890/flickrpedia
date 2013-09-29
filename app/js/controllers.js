d19ac6e9612eec09d4ade93341ed7893fdb87832
Dave McCloskey <davemccloskey@gmail.com>
2013-09-28 19:49:58 -0700

    add placeholder images so the page doesn't jitter while loading

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,4 +14,8 @@ angular.module('flickrpedia.controllers', ['ngResource']).
       'title': 'Cupcakes',
       'text': 'Cotton candy donut pie liquorice tart pie jelly-o bonbon oat cake. Pastry liquorice toffee chocolate bar caramels toffee lemon drops jelly-o halvah. Lollipop candy canes jujubes jelly beans. Lollipop muffin bonbon biscuit lemon drops bonbon apple pie. Apple pie powder caramels dragée. Candy canes cotton candy pudding caramels pastry cupcake. Donut bear claw lollipop tootsie roll gingerbread marzipan brownie. Gummi bears brownie croissant soufflé pie gingerbread. Tiramisu brownie liquorice sugar plum. Carrot cake jelly-o chupa chups topping danish. Chupa chups fruitcake bear claw macaroon dessert donut. Sweet roll unerdwear.com marzipan muffin jelly beans macaroon chocolate toffee.'
     };
+    $scope.results = [];
+    for (var i = 0; i < 10; i++) {
+      $scope.results.push({url: '/img/1x1.gif'});
+    }
   }]);
