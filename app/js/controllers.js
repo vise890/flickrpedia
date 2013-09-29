f811e4ebbb366d3573c99997d1039143931594af
Dave McCloskey <davemccloskey@gmail.com>
2013-09-28 17:11:43 -0700

    initial structure for page

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,9 +5,40 @@
 angular.module('flickrpedia.controllers', []).
   controller('Main', ['$scope', '$routeParams', function($scope, $routeParams) {
     $scope.term = $routeParams.term;
+    $scope.article = {
+      'title': 'San Francisco',
+      'text': 'San Francisco, officially the City and County of San Francisco, '
+        + 'is the leading financial and cultural center of Northern California '
+        + 'and the San Francisco Bay Area. The only consolidated city-county in'
+        + ' California, San Francisco encompasses a land area of about 46.9 '
+        + 'square miles (121 km2)[12] on the northern end of the San Francisco '
+        + 'Peninsula, giving it a density of about 17,620 people per square '
+        + 'mile (6,803 people per km2). It is the most densely settled large '
+        + 'city (population greater than 200,000) in the state of California '
+        + 'and the second-most densely populated major city in the United '
+        + 'States after New York City. San Francisco is the fourth most '
+        + 'populous city in California, after Los Angeles, San Diego and San '
+        + 'Jose, and the 14th most populous city in the United States—with a '
+        + 'Census-estimated 2012 population of 825,863.[14] The city is also '
+        + 'the financial and cultural hub of the larger San Jose-San '
+        + 'Francisco-Oakland metropolitan area, with a population of 8.4 '
+        + 'million.'
+    }
     $scope.results = [
       {
         url: 'http://farm6.staticflickr.com/5248/5229241621_75fddaa50e_b.jpg'
+      },
+      {
+        url: 'http://farm5.staticflickr.com/4005/4204497503_cacf3abe74_o.jpg'
+      },
+      {
+        url: 'http://farm5.staticflickr.com/4005/4204497503_cacf3abe74_o.jpg'
+      },
+      {
+        url: 'http://farm5.staticflickr.com/4005/4204497503_cacf3abe74_o.jpg'
+      },
+      {
+        url: 'http://farm5.staticflickr.com/4005/4204497503_cacf3abe74_o.jpg'
       },
       {
         url: 'http://farm5.staticflickr.com/4005/4204497503_cacf3abe74_o.jpg'
