f3d3473af4ee2ada58c11159e9d9dc2b2a4dd7ed
Dave McCloskey <davemccloskey@gmail.com>
2013-09-28 23:40:06 -0700

    this stupid fucking search bar is finally doing something

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,10 +2,13 @@
 
 /* Directives */
 
-
-angular.module('flickrpedia.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('flickrpedia.directives', [])
+  .directive('onKeyup', function($location) {
     return function(scope, elm, attrs) {
-      elm.text(version);
+        elm.bind('keyup', function(evt) {
+          if (evt.which == 13) {
+            window.location.href = '#/' + this.value;
+          }
+        });
     };
-  }]);
+});
